f9eaec4bde3530ca3658d2e9d0b581ea03365b21
ruofei <rf.wangchn@foxmail.com>
2021-03-29 12:01:47 +0800

    chore: 配置 tcb

---
+++ _init_tcb-env.js
@@ -0,0 +1,6 @@
+window._tcbEnv = {
+  TCB_SERVICE_DOMAIN:
+    "hexo-blog-1gqvz4yy54676e17-1257250882.ap-shanghai.service.tcloudbase.com",
+  TCB_ENV_ID: "hexo-blog-1gqvz4yy54676e17",
+  TCB_REGION: "ap-shanghai",
+};
